67a507cd511bb6013123e82816a9d9c1f4a598f3
HengBinXu <bin801027@gmail.com>
2021-11-06 16:33:44 +0800

    Remove redundant header from MopsSpider

--- financial_reports/spiders/__init__.py
+++ financial_reports/spiders/__init__.py
@@ -12,22 +12,7 @@ from ..company_list import CompanyList
 class MopsSpider(Spider):
     
     name = None
-
-    headers = {
-        'User-Agent': (
-            'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) '
-            'AppleWebKit/537.36 (KHTML, like Gecko) Chrome/95.0.4638.69 Safari/537.36'
-        ),
-        'Accept': (
-            'text/html,application/xhtml+xml,'
-            'application/xml;q=0.9,image/avif,image/webp,'
-            'image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9'
-        ),
-        'Accept-Encoding': 'gzip, deflate, br',
-        'Accept-Language': 'en-US,en;q=0.9,zh-TW;q=0.8,zh;q=0.7',
-        'Host': 'emops.twse.com.tw',
-    }
-    
+        
     # TODO:
     # Need to add year
     @property
