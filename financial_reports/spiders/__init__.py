71989b1cdd0d14f2ca96796195bc53a6d40a2777
HengBinXu <bin801027@gmail.com>
2021-11-15 00:45:57 +0800

    Finish balance sheet spider

--- financial_reports/spiders/__init__.py
+++ financial_reports/spiders/__init__.py
@@ -2,6 +2,7 @@
 #
 # Please refer to the documentation for information on how to create and manage
 # your spiders.
+import re
 
 from scrapy import Spider
 from scrapy import Request
@@ -12,6 +13,27 @@ from ..company_list import CompanyList
 class MopsSpider(Spider):
     
     name = None
+
+    # Define data clean functions
+    def to_lowercase(self, value: str) -> str:
+        return value.lower()
+
+    def remove_space(self, value: str) -> str:
+        return value.strip()
+
+    def replace_symbol(self, value: str) -> str:
+        return value.replace('-', '_')
+
+    def replace_space(self, value: str) -> str:
+        return value.replace(' ', '_')
+
+    def remove_comma(self, value: str) -> str:
+        return value.replace(',', '')
+
+    def process_unit(self, value: str) -> str:
+        replace_pat = re.compile(r'Unit:\s')
+        value = replace_pat.sub('', value)
+        return value.replace(' ', ',')
 
     @property
     def start_urls(self):
@@ -25,7 +47,5 @@ class MopsSpider(Spider):
         for url_info in self.start_urls:
             url = url_info['request_url']
             method = url_info['method']
-            print(url_info)
-            input('-' * 100)
             yield Request(url, callback=self.parse, method=method, headers=self.headers)
             
--- financial_reports/spiders/balance_sheet.py
+++ financial_reports/spiders/balance_sheet.py
@@ -1,8 +1,115 @@
 from . import MopsSpider
+from scrapy import Request
+from scrapy.loader import ItemLoader
+
+from ..items import BalanceSheetItem
 
 class BalanceSheet(MopsSpider):
     
     name = "balance_sheet"
-    
+
+    # Below are for test
+    # start_urls = [
+    #     'https://emops.twse.com.tw/server-java/t164sb03_e?TYPEK=all&step=show&co_id=2330&year=2020&season=4&report_id=C'
+    # ]
+
+    # def start_requests(self):
+    #     for url in self.start_urls:
+    #         request_info = {
+    #             'company_id': '2330',
+    #             'year': 2020,
+    #             'season': 4,
+    #         }
+    #         yield Request(url, callback=self.parse, method='GET', cb_kwargs=request_info)
+
+    def subject_processor(self, value: str) -> str:
+        process_funcs = [
+            self.remove_space,
+            self.to_lowercase,
+            self.replace_space,
+            self.replace_symbol,
+        ]
+        for func in process_funcs:
+            value = func(value)
+        return value
+
+    def value_processor(self, value: str) -> str:
+        process_funcs = [
+            self.remove_space,
+            self.remove_comma,
+        ]
+        for func in process_funcs:
+            value = func(value)
+        return value
+
+    def subject_reference(self, value: str) -> dict:
+        reference = {
+            'current_assets': {
+                'component': 'assets', 'aggregate_subject': 'total_assets'
+            },
+            'non_current_assets': {
+                'component': 'assets', 'aggregate_subject': 'total_assets'
+            },
+            'current_liabilities': {
+                'component': 'liabilities', 'aggregate_subject': 'total_liabilities'
+            },
+            'non_current_liabilities': {
+                'component': 'liabilities', 'aggregate_subject': 'total_liabilities'
+            },
+            'equity_attributable_to_owners_of_parent': {
+                'component': 'equity', 'aggregate_subject': 'total_equity'
+            },
+        }
+        refer_info = reference.get(value, None)
+        return refer_info
+
     def parse(self, response, **kwargs):
-        pass
+        unit = response.css('.in-w-10::text').getall()[-1]
+        unit = self.process_unit(unit)
+        table_rows = response.css('.hasBorder > tr:not([class="bl-d-12"])')
+        reset_component = False
+        component, category, sub_category, subject = ['', '', '', '']
+        for row in table_rows:
+            balance_sheet_item = BalanceSheetItem()
+            balance_sheet_item['company_id'] = kwargs['company_id']
+            balance_sheet_item['year'] = kwargs['year']
+            balance_sheet_item['season'] = kwargs['season']
+            balance_sheet_item['unit'] = unit
+            # Extract data from each row
+            row_values = row.css('td::text').getall()
+            if len(row_values) == 1:
+                if component == 'equity':
+                    # Only equity has sub_category.
+                    sub_category = self.subject_processor(row_values[0])
+                else:
+                    category = self.subject_processor(row_values[0])
+                    refer_info = self.subject_reference(category)
+                    component = refer_info['component']
+                continue
+
+            subject, value, _ = row_values
+            subject = self.subject_processor(subject)
+            value = self.value_processor(value)
+
+            # The aggregate subject doesn't belong to any category and sub_category.
+            if subject == refer_info['aggregate_subject']:
+                reset_component = True
+                category, sub_category = '', ''
+
+            balance_sheet_item['component'] = component
+            balance_sheet_item['category'] = category
+            balance_sheet_item['sub_category'] = sub_category
+            balance_sheet_item['subject'] = subject
+            balance_sheet_item['value'] = value
+
+            # After aggregate subject, it needs to reset component.
+            if reset_component:
+                component = ''
+                reset_component = True
+
+            yield balance_sheet_item
+
+# Balance Sheet:
+#   formula => Assets = Liabilities + Shareholder Equity
+# Reference:
+# https://www.investopedia.com/terms/b/balancesheet.asp
