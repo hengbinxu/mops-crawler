14597253697652377bb27794841d84497333652d
HengBinXu <bin801027@gmail.com>
2021-11-15 00:48:15 +0800

    Modify balance sheet item model.

--- financial_reports/items.py
+++ financial_reports/items.py
@@ -13,6 +13,33 @@ class TWFiftyItem(scrapy.Item):
     weight = Field()
 
 
+class CompanyInfoItem(scrapy.Item):
+    market = Field()
+    company_id = Field()
+    company_name = Field()
+    place_of_incorporation_of_foreign_companies = Field()
+    industry = Field()
+    symbol = Field()
+    date_of_listing = Field()
+    chariman = Field()
+    general_manager = Field()
+    spokesman = Field()
+    title_of_spokersman = Field()
+    fiscal_year_end = Field()
+    accounting_firm = Field()
+    cpa_1 = Field()
+    cpa_2 = Field()
+    address = Field()
+    telephone = Field()
+    fax = Field()
+    email = Field()
+    web_address = Field()
+    investor_relations_contact_person = Field()
+    title_of_ir_contact_person = Field()
+    telephon_of_ir_contact_person = Field()
+    email_of_ir_contact_person = Field()
+
+
 class BaseItem(scrapy.Item):
     company_id = Field()
     year = Field()
@@ -21,8 +48,9 @@ class BaseItem(scrapy.Item):
 
 
 class BalanceSheetItem(BaseItem):
+    component = Field()
     category = Field()
-    sub_categiry = Field()
+    sub_category = Field()
     subject = Field()
     value = Field()
 
